986677952fb50fba99fcfddaead8561a4a00d6d5
hwang-ua <han8@ualberta.ca>
2024-05-31 16:48:22 -0600

    configs

--- configs/config_v0.py
+++ configs/config_v0.py
@@ -5,14 +5,15 @@ from utils import write_job_scripts, add_seed_scripts, add_param_scripts, policy
 def c20240529():
     sweep = {
         " --pi_lr ": [1e-3, 3e-4, 1e-4],
-        " --tau ": [0.01, 0.1, 0.3],
+        # " --tau ": [0.01, 0.1, 0.3],
+        " --tau ": [1, 5, 10],
         " --rho ": [0.2],
         " --q_lr_prob ": [1.],
         " --info ": ["test_v0"],
     }
-    target_agents = ["FTT_AWAC", "TTT_AWAC"]
+    target_agents = ["FTT_AWAC"]
     target_envs = ["Hopper"]
-    target_datasets = ["expert", "medium"]
+    target_datasets = ["expert"]
     target_distributions = ["qGaussian"]
     write_job_scripts(sweep, target_agents, target_envs, target_datasets, target_distributions, num_runs=5, comb_num_base=0, prev_file=0, line_per_file=1)
 
